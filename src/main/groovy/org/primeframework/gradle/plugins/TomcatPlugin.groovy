3acd191ae94aa4da010d1766306153518af31f78
James Humphrey <leafknode@gmail.com>
2012-11-13 09:57:53 -0700

    small change to log message

--- src/main/groovy/org/primeframework/gradle/plugins/TomcatPlugin.groovy
+++ src/main/groovy/org/primeframework/gradle/plugins/TomcatPlugin.groovy
@@ -1,9 +1,9 @@
 package org.primeframework.gradle.plugins
 
 import org.apache.commons.io.FileUtils
-import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.GradleException
 
 /**
  * The database plugin is used for creating database
@@ -18,7 +18,7 @@ class TomcatPlugin implements Plugin<Project> {
 
     project.task("tomcat", dependsOn: ["war"]) << {
 
-      println "Setting up project to run in tomcat..."
+      println "Setting up project to run tomcat in ${project.buildDir}/tomcat"
 
       def catalinaHome = System.getenv("CATALINA_HOME")
 
